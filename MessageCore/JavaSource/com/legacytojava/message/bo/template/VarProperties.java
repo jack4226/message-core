9df295465eef8d92390ed0e1956d65da2a87a9e3
jackwng@gmail.com <jackwng@gmail.com@0b04287a-0cf2-6031-b5ca-028f388d26b3>
2013-03-01 13:41:18 +0000

    Updated.

--- MessageCore/JavaSource/com/legacytojava/message/bo/template/VarProperties.java
+++
@@ -1,7 +0,0 @@
-package com.legacytojava.message.bo.template;
-
-public class VarProperties {
-	int bgnPos = 0;
-	int endPos = 0;
-	String name = null;
-}
--- MessageCore/JavaSource/com/legacytojava/message/dao/customer/CustSequenceDao.java
+++
@@ -1,5 +0,0 @@
-package com.legacytojava.message.dao.customer;
-
-public interface CustSequenceDao {
-	public long findNextValue();
-}
