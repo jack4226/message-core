38a0e1e439d2f0b13f164404af7eccbc19871832
wangj3 <wangj3@LNGRDUL-7009296.legal.regn.net>
2016-12-06 09:40:43 -0500

    Signed-off-by: wangj3 <wangj3@LNGRDUL-7009296.legal.regn.net>

--- MessageCore/MessageCore/JavaSource/com/legacytojava/message/vo/package-info.java
+++
@@ -1,5 +0,0 @@
-@XmlJavaTypeAdapter(value=TimestampAdapter.class,type=Timestamp.class)
-package com.legacytojava.message.vo;
-import java.sql.Timestamp;
-
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
