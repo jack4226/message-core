1fb20c06acd64a5121bd683a630752130c14884e
jwang <jwang@mac-h97i.nc.rr.com>
2016-12-09 12:32:47 -0500

    Signed-off-by: jwang <jwang@mac-h97i.nc.rr.com>

---
+++ MessageCore1/src/main/java/ltj/message/bo/mailreader/MailReaderApp.java
@@ -0,0 +1,31 @@
+package ltj.message.bo.mailreader;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import ltj.jbatch.app.SpringUtil;
+import ltj.message.dao.mailbox.MailBoxDao;
+import ltj.message.vo.MailBoxVo;
+
+public class MailReaderApp {
+	static final Logger logger = Logger.getLogger(MailReaderApp.class);
+	
+	public static void main(String[] args) {
+		AbstractApplicationContext factory = SpringUtil.getAppContext();
+		MailBoxDao mailBoxDao = factory.getBean(MailBoxDao.class);
+		MailBoxVo vo = mailBoxDao.getByPrimaryKey("jwang", "localhost");
+		if (vo == null) return;
+		vo.setFromTimer(true);
+		MailReaderBoImpl reader = new MailReaderBoImpl(vo, factory);
+		try {
+				//reader.start();
+				//reader.join();
+				reader.readMail(vo.isFromTimer());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.exit(0);
+	}
+
+}
