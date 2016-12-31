ac45f4654fb8c505623cf08a90eb24d31837fcc0
jwang <jwang@macbook-840g1.nc.rr.com>
2016-12-30 23:14:35 -0500

    Signed-off-by: jwang <jwang@macbook-840g1.nc.rr.com>

--- MessageCore1/src/test/java/ltj/message/bo/RuleDataBoTest.java
+++ MessageCore1/src/test/java/ltj/message/bo/RuleDataBoTest.java
@@ -6,14 +6,17 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 
 import ltj.message.bo.rule.RulesDataBo;
 import ltj.message.bo.test.BoTestBase;
+import ltj.message.constant.RuleNameType;
 import ltj.message.dao.rule.RuleDao;
 import ltj.message.util.PrintUtil;
+import ltj.message.vo.rule.RuleElementVo;
 import ltj.message.vo.rule.RuleVo;
 
 public class RuleDataBoTest extends BoTestBase {
@@ -26,19 +29,68 @@ public class RuleDataBoTest extends BoTestBase {
 	private RuleDao ruleDao;;
 
 	private List<RuleVo> ruleList;
+	private String postmasterEexclusions;
+	private String subsTargerText;
 	
 	@Before
 	public void setup() {
 		ruleList = ruleDao.getActiveRules();
+		for (RuleVo vo : ruleList) {
+			if (RuleNameType.HARD_BOUNCE.name().equals(vo.getRuleName())) {
+				for (RuleElementVo elmVo : vo.getRuleElementVos()) {
+					if ("excludingPostmastersBo".equals(elmVo.getExclListProc())) {
+						postmasterEexclusions = elmVo.getExclusions();
+						break;
+					}
+				}
+			}
+			else if (RuleNameType.SUBSCRIBE.name().equals(vo.getRuleName())) {
+				for (RuleElementVo elmVo : vo.getRuleElementVos()) {
+					if ("mailingListRegExBo".equals(elmVo.getTargetProc())) {
+						subsTargerText = elmVo.getTargetText();
+					}
+				}
+			}
+		}
+		assertNotNull(postmasterEexclusions);
+		assertNotNull(subsTargerText);
 	}
 	
 	@Test
 	public void test() {
 		List<RuleVo> rules = rulesDataBo.getCurrentRules();
+		String all_exclusions = null;
+		String new_targetText = null;
 		for (RuleVo vo : rules) {
 			assertTrue(containsRule(vo.getRuleName()));
-			logger.info(PrintUtil.prettyPrint(vo, 2));
+			if (RuleNameType.HARD_BOUNCE.name().equals(vo.getRuleName())) {
+				logger.info(PrintUtil.prettyPrint(vo, 2));
+				for (RuleElementVo elmVo : vo.getRuleElementVos()) {
+					if ("excludingPostmastersBo".equals(elmVo.getExclListProc())) {
+						all_exclusions = elmVo.getExclusions();
+						break;
+					}
+				}
+			}
+			else if (RuleNameType.SUBSCRIBE.name().equals(vo.getRuleName())) {
+				logger.info(PrintUtil.prettyPrint(vo, 2));
+				for (RuleElementVo elmVo : vo.getRuleElementVos()) {
+					if ("mailingListRegExBo".equals(elmVo.getTargetProc())) {
+						new_targetText = elmVo.getTargetText();
+					}
+				}
+			}
+			else {
+				//logger.info(PrintUtil.prettyPrint(vo, 2));
+			}
 		}
+		// verify exclusions
+		assertNotNull(all_exclusions);
+		assertTrue(StringUtils.contains(all_exclusions, postmasterEexclusions));
+		assertNotEquals(all_exclusions, postmasterEexclusions);
+		// verify target text override 
+		assertNotNull(new_targetText);
+		assertNotEquals(subsTargerText, new_targetText);
 	}
 
 	boolean containsRule(String ruleName) {
