c4acb8b1ddd0beb4fea76b3de010d04ca362b9fe
wangj3 <wangj3@LNGRDUL-7009296.legal.regn.net>
2017-01-17 11:12:05 -0500

    Signed-off-by: wangj3 <wangj3@LNGRDUL-7009296.legal.regn.net>

--- MsgUI1/src/ltj/msgui/bean/PhaseTrackerSetterBean.java
+++ MsgUI1/src/ltj/msgui/bean/PhaseTrackerSetterBean.java
@@ -9,38 +9,35 @@ import javax.faces.lifecycle.LifecycleFactory;
 import javax.faces.model.SelectItem;
 
 public class PhaseTrackerSetterBean {
-   private SelectItem[] phases = {
-      new SelectItem("RESTORE_VIEW"),
-      new SelectItem("APPLY_REQUEST_VALUES"),
-      new SelectItem("PROCESS_VALIDATIONS"),
-      new SelectItem("UPDATE_MODEL_VALUES"),
-      new SelectItem("INVOKE_APPLICATION"),
-      new SelectItem("RENDER_RESPONSE"),
-      new SelectItem("ANY_PHASE"),
-   };
+	private SelectItem[] phases = { new SelectItem("RESTORE_VIEW"), new SelectItem("APPLY_REQUEST_VALUES"),
+			new SelectItem("PROCESS_VALIDATIONS"), new SelectItem("UPDATE_MODEL_VALUES"),
+			new SelectItem("INVOKE_APPLICATION"), new SelectItem("RENDER_RESPONSE"), new SelectItem("ANY_PHASE"), };
 
-   public SelectItem[] getPhases() { return phases; }
+	public SelectItem[] getPhases() {
+		return phases;
+	}
 
-   public void phaseChange(ValueChangeEvent e) {
-      LifecycleFactory factory = (LifecycleFactory) FactoryFinder.getFactory(
-            FactoryFinder.LIFECYCLE_FACTORY);
-      Lifecycle lifecycle = factory.getLifecycle(LifecycleFactory.
-            DEFAULT_LIFECYCLE);
+	public void phaseChange(ValueChangeEvent e) {
+		LifecycleFactory factory = (LifecycleFactory) FactoryFinder.getFactory(FactoryFinder.LIFECYCLE_FACTORY);
+		Lifecycle lifecycle = factory.getLifecycle(LifecycleFactory.DEFAULT_LIFECYCLE);
 
-      PhaseListener[] listeners = lifecycle.getPhaseListeners();
-      for (int i = 0; i < listeners.length; i++) {
-         PhaseListener listener = listeners[i]; 
-         if (listener instanceof ltj.msgui.listener.PhaseTracker)
-            ((ltj.msgui.listener.PhaseTracker) listener).setPhase(
-                  (String) e.getNewValue());
-      }
-   }
+		PhaseListener[] listeners = lifecycle.getPhaseListeners();
+		for (int i = 0; i < listeners.length; i++) {
+			PhaseListener listener = listeners[i];
+			if (listener instanceof ltj.msgui.listener.PhaseTracker) {
+				((ltj.msgui.listener.PhaseTracker) listener).setPhase((String) e.getNewValue());
+			}
+		}
+	}
+
 	public void afterPhase(PhaseEvent event) {
 		System.out.println("AFTER PHASE " + showEvent(event));
-   }
+	}
+
 	public void beforePhase(PhaseEvent event) {
 		System.out.println("BEFORE PHASE " + showEvent(event));
-   }
+	}
+
 	private String showEvent(PhaseEvent event) {
 		return "Phase Event: " + event.getPhaseId();
 	}
