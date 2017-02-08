ad0d387bf5eb7711764e65f1333c14090fc7d688
wangj3 <wangj3@LNGRDUL-7009296.legal.regn.net>
2017-02-08 14:21:03 -0500

    Signed-off-by: wangj3 <wangj3@LNGRDUL-7009296.legal.regn.net>

--- MessageCore1/src/main/java/ltj/message/constant/Constants.java
+++ MessageCore1/src/main/java/ltj/message/constant/Constants.java
@@ -1,6 +1,6 @@
 package ltj.message.constant;
 
-public class Constants {
+public final class Constants {
 	
 	public final static String DEFAULT_USER_ID = "MsgMaint";
 	public final static String DEFAULT_DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
@@ -8,8 +8,9 @@ public class Constants {
 	
 	public final static String VENDER_DOMAIN_NAME = "Emailsphere.com";
 	public final static String VENDER_SUPPORT_EMAIL = "support@" + VENDER_DOMAIN_NAME;
-	public final static String POWERED_BY_HTML_TAG = "<div style='color: blue;'>Powered by " +
-			"<a style='color: darkblue;' href='http://www." + VENDER_DOMAIN_NAME + "' target='_blank'>"+ VENDER_DOMAIN_NAME + "</a></div>";
+	public final static String POWERED_BY_HTML_TAG = "<div style='color: blue;'>Powered by "
+			+ "<a style='color: darkblue;' href='http://www." + VENDER_DOMAIN_NAME + "' target='_blank'>"
+			+ VENDER_DOMAIN_NAME + "</a></div>";
 	public final static String POWERED_BY_TEXT = "Powered by " + VENDER_DOMAIN_NAME;
 	public static final boolean EmbedPoweredByToFreeVersion = true;
 	
