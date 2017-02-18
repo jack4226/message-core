92b5c584b0f44e6095b1d7635118e26415ba4905
jwang <jwang@macbook-840g2.nc.rr.com>
2017-02-17 23:23:06 -0500

    Signed-off-by: jwang <jwang@macbook-840g2.nc.rr.com>

--- MessageCore1/src/main/java/ltj/message/constant/StatusId.java
+++ MessageCore1/src/main/java/ltj/message/constant/StatusId.java
@@ -5,16 +5,38 @@ public enum StatusId {
 	//
 	// define general statusId
 	//
-	ACTIVE("A"),
-	INACTIVE("I"),
-	SUSPENDED("S"); // for Email Address
+	ACTIVE("A", IdType.Record),
+	INACTIVE("I", IdType.Record),
+	SUSPENDED("S", IdType.Record), // for Email Address
 
+	//
+	// define out-bound message statusId
+	//
+	PENDING("P", IdType.Outbound),
+	DELIVERED("D", IdType.Outbound),
+	DELIVERY_FAILED("F", IdType.Outbound),
+	//
+	// define in-bound message status
+	//
+	CLOSED("C", IdType.Inbound),
+	OPENED("O", IdType.Inbound);
+	
 	private final String value;
-	private StatusId(String value) {
+	private final IdType type;
+	private StatusId(String value, IdType type) {
 		this.value = value;
+		this.type = type;
 	}
 	
 	public String value() {
 		return value;
+	}
+	
+	public IdType getType() {
+		return type;
+	}
+	
+	public static enum IdType {
+		Record, Inbound, Outbound;
 	}
 }
