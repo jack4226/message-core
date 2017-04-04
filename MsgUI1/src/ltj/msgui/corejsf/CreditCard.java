aa5b17168b583d4803cdd14af74120e0fbd328ba
jwang <jwang@192.168.1.71>
2017-04-04 19:37:10 -0400

    Signed-off-by: jwang <jwang@192.168.1.71>

--- MsgUI1/src/ltj/msgui/corejsf/CreditCard.java
+++
@@ -1,8 +0,0 @@
-package ltj.msgui.corejsf;
-
-public class CreditCard {
-   private String number;
-
-   public CreditCard(String number) { this.number = number; }
-   public String toString() { return number; }
-}
