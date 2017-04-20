39f175e3c1f43fdb1d7420a64b7fef483c61e699
jwang <jwang@H97I-HTPC.localdomain>
2017-04-20 13:54:38 -0400

    Signed-off-by: jwang <jwang@H97I-HTPC.localdomain>

--- MsgTest/src/test/java/ltj/msgui2/BuiltinRuleTest.java
+++ MsgTest/src/test/java/ltj/msgui2/BuiltinRuleTest.java
@@ -1,97 +1,107 @@
-package ltj.selenium;
+package ltj.msgui2;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
-import java.util.Random;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.BlockJUnit4ClassRunner;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import junit.framework.TestCase;
+public class BuiltinRuleTest extends AbstractLogin {
+	static final Logger logger = Logger.getLogger(BuiltinRuleTest.class);
 
-@RunWith(BlockJUnit4ClassRunner.class)
-public class LoginFlowTest extends TestCase {
-	static final Logger logger = Logger.getLogger(LoginFlowTest.class);
-
-	private static ChromeDriverService service;
-	private WebDriver driver;
-
-	@BeforeClass
-	public static void createAndStartService() throws IOException {
-		service = new ChromeDriverService.Builder().usingDriverExecutable(new File(OSUtil.getChromeDriverPath()))
-				.usingAnyFreePort().build();
-		service.start();
-	}
-
-	@AfterClass
-	public static void createAndStopService() {
-		service.stop();
-	}
-
-	@Before
-	public void createDriver() {
-		driver = new RemoteWebDriver(service.getUrl(), DesiredCapabilities.chrome());
-		login();
-	}
-
-	@After
-	public void quitDriver() {
-		driver.quit();
-	}
-
-	void login() {
+	@Test
+	public void testListAndViewDetail() {
+		logger.info("Current URL: " + driver.getCurrentUrl());
+		String listTitle = "Customize Actions for Built-in Rules";
 		try {
-			driver.get("http://localhost:8080/MsgUI2/login.faces");
-	
-			// Find the text input element by its name
-			WebElement element_user = driver.findElement(By.id("login:userid"));
-			element_user.sendKeys("admin");
+			WebElement link = driver.findElement(By.linkText(listTitle));
+			link.click();
 			
-			WebElement element_pswd = driver.findElement(By.name("login:password"));
-			element_pswd.sendKeys("admin");
+			WebDriverWait wait = new WebDriverWait(driver, 5);
+			wait.until(ExpectedConditions.titleIs(listTitle));
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.id("builtinlst:footer:gettingStartedFooter")));
 			
-			List<WebElement> inputs = driver.findElements(By.xpath("//input"));
-			for (WebElement we : inputs) {
-				logger.info("Input: " + we.toString());
+			logger.info("Switched to URL: " + driver.getCurrentUrl());
+			
+			// Retrieve field values from the list
+			WebElement ruleNameElm = driver.findElement(By.cssSelector("span[title='Hard Bounce_ruleName']"));
+			String ruleNameBefore = ruleNameElm.getText();
+			
+			WebElement ruleTypeElm = driver.findElement(By.cssSelector("span[title='Hard Bounce_ruleType']"));
+			String ruleTypeBefore = ruleTypeElm.getText();
+			
+			WebElement categoryElm = driver.findElement(By.cssSelector("span[title='Hard Bounce_category']"));
+			String categoryBefore = categoryElm.getText();
+			
+			WebElement viewActionsLink = driver.findElement(By.cssSelector("a[title='Hard Bounce_viewActions']"));
+			viewActionsLink.click();
+			
+			logger.info("Edit page URL: " + driver.getCurrentUrl());
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.id("actionbiedit:footer:gettingStartedFooter")));
+			
+			// View/Edit Detail Page
+			WebElement ruleNameEdt = driver.findElement(By.id("actionbiedit:content:rulename"));
+			assertEquals(ruleNameBefore, ruleNameEdt.getText());
+			
+			WebElement ruleTypeEdt = driver.findElement(By.id("actionbiedit:content:ruletype"));
+			assertEquals(ruleTypeBefore, ruleTypeEdt.getText());
+			
+			WebElement categoryEdt = driver.findElement(By.id("actionbiedit:content:rulecategory"));
+			assertEquals(categoryBefore, categoryEdt.getText());
+			
+			WebElement seqEdt = driver.findElement(By.id("actionbiedit:content:builtin:0:actionseq"));
+			assertEquals("1", seqEdt.getAttribute("value"));
+			
+			List<WebElement> actSeqList = driver.findElements(By.cssSelector("input[id$=':actionseq']"));
+			assertFalse(actSeqList.isEmpty());
+			
+			List<WebElement> actIdList = driver.findElements(By.cssSelector("select[id$=':actionid']"));
+			assertEquals(actSeqList.size(), actIdList.size());
+			for (int i = 0; i < actIdList.size(); i++) {
+				Select selectActId = new Select(actIdList.get(i));
+				WebElement selectedActId = selectActId.getFirstSelectedOption();
+				if (i == 0) {
+					assertEquals("SAVE", selectedActId.getAttribute("value"));
+				}
+				else if (i == 1) {
+					assertEquals("SUSPEND", selectedActId.getAttribute("value"));
+				}
+				else if (i == 3) {
+					assertEquals("CLOSE", selectedActId.getAttribute("value"));
+				}
 			}
 			
-			//element_pswd.submit();
-			driver.findElement(By.id("login:submit")).click();
-	
-			logger.info("Page title is: " + driver.getTitle());
-	
-			logger.info("Page URL is: " + driver.getCurrentUrl());
-			logger.info("Page source is: " + driver.getPageSource());
-	        
-			// wait until page is loaded
-			WebDriverWait wait = new WebDriverWait(driver, 5);
-			wait.until(ExpectedConditions.titleIs("Main Page"));
+			List<WebElement> statIdList = driver.findElements(By.cssSelector("select[id$=':statusid']"));
+			assertEquals(actSeqList.size(), statIdList.size());
+			for (WebElement elm : statIdList) {
+				Select selectStatId = new Select(elm);
+				WebElement selectedStatId = selectStatId.getFirstSelectedOption();
+				assertEquals("A", selectedStatId.getAttribute("value"));
+			}
+			
+			Select selectValues = new Select(driver.findElement(By.id("actionbiedit:content:builtin:1:datatypevalues1")));
+			List<WebElement> selectedValues = selectValues.getAllSelectedOptions();
+			assertFalse(selectedValues.isEmpty());
+			assertEquals("$FinalRcpt", selectedValues.get(0).getAttribute("value"));
+			
+			// Go back to list
+			WebElement submit = driver.findElement(By.cssSelector("input[title='Go Back']"));
+			submit.click();
+
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.id("builtinlst:footer:gettingStartedFooter")));
+			
 		}
 		catch (Exception e) {
 			logger.error("Exception caught", e);
 			fail();
 		}
 	}
-	
+
 	@Test
 	public void testAddNewAction() {
 		logger.info("Current URL: " + driver.getCurrentUrl());
@@ -193,87 +203,5 @@ public class LoginFlowTest extends TestCase {
 			fail();
 		}
 	}
-	
-	/*
-	 Configure Site Profiles
-	 Configure POP/IMAP Accounts
-	 Configure SMTP Servers
-	 
-	 Customize Actions for Built-in Rules
-	 Setup Custom Bounce Rules and Actions
-	 Customize Action Details
-	 
-	 Setup Email Mailing Lists
-	 Upload Email Addresses to List
-	 Setup Email Variables
-	 Setup Email Templates
-	 
-	 Manage Email Correspondence
-	 Broadcast to a Mailing List
-	 Manage Email Addresses
-	 Manage Customer Information
-	 View Broadcast Messages
-	 
-	 Manage Site Users
-	 Change Password
-	 */
-	
-	
-	public static void main(String[] args) {
-		OSUtil.setupSeleniumBrowserDriver();
-		
-		WebDriver driver = new ChromeDriver();
-		//WebDriver driver = new FirefoxDriver();
-		
-		try {
-			driver.get("http://localhost:8080/MsgUI1/login.faces");
-	
-			// Find the text input element by its name
-			WebElement element_user = driver.findElement(By.id("login:userid"));
-			element_user.sendKeys("admin");
-			
-			WebElement element_pswd = driver.findElement(By.name("login:password"));
-			element_pswd.sendKeys("admin");
-			
-			List<WebElement> inputs = driver.findElements(By.xpath("//input"));
-			for (WebElement we : inputs) {
-				logger.info("Input: " + we.toString());
-			}
-			
-			// Now submit the form. WebDriver will find the form for us from the element
-			//element_pswd.submit();
-			driver.findElement(By.id("login:submit")).click();
-	
-			// Check the title of the page
-			logger.info("Page title is: " + driver.getTitle());
-			logger.info("Page URL is: " + driver.getCurrentUrl());
-			logger.info("Page source is: " + driver.getPageSource());
 
-			// wait until page is loaded
-			WebDriverWait wait = new WebDriverWait(driver, 5);
-			wait.until(ExpectedConditions.titleIs("Main Page"));
-			
-			logger.info("Current URL: " + driver.getCurrentUrl());
-		}
-		catch (Exception e) {
-			logger.error("Exception caught", e);
-		}
-		finally {
-			// Close the browser
-			driver.quit();
-		}
-	}
-	
-	/*
-	 * Start the ChromeDriver server separately before running tests: 
-	 * Terminal:
-		$ ./chromedriver
-		Started ChromeDriver
-		port=9515
-		version=14.0.836.0
-	 * 
-	 * and connect to it using the Remote WebDriver:
-	 * WebDriver driver = new RemoteWebDriver("http://127.0.0.1:9515", DesiredCapabilities.chrome());
-	 * driver.get("http://www.google.com");
-	 */
 }
--- MsgTest/src/test/java/ltj/msgui2/SmtpServerTest.java
+++ MsgTest/src/test/java/ltj/msgui2/SmtpServerTest.java
@@ -17,6 +17,112 @@ public class SmtpServerTest extends AbstractLogin {
 	static final Logger logger = Logger.getLogger(SmtpServerTest.class);
 
 	@Test
+	public void testListAndViewDetail() {
+		logger.info("Current URL: " + driver.getCurrentUrl());
+		String listTitle = "Configure SMTP Servers";
+		try {
+			WebElement link = driver.findElement(By.linkText(listTitle));
+			link.click();
+			
+			WebDriverWait wait = new WebDriverWait(driver, 5);
+			wait.until(ExpectedConditions.titleIs(listTitle));
+			
+			logger.info("Switched to URL: " + driver.getCurrentUrl());
+			
+			// save field values from the list
+			WebElement checkBoxElm = driver.findElement(By.cssSelector("input[title='gmailServer_checkBox']"));
+			assertEquals(true, checkBoxElm.isDisplayed());
+			
+			WebElement serverNameLink = driver.findElement(By.cssSelector("a[title='gmailServer_viewDetail']"));
+			String serverNameBefore = serverNameLink.getText();
+			
+			WebElement hostNameElm = driver.findElement(By.cssSelector("span[title='gmailServer_hostName']"));
+			String hostNameBefore = hostNameElm.getText();
+			
+			WebElement userIdElm = driver.findElement(By.cssSelector("span[title='gmailServer_userId']"));
+			String userIdBefore = userIdElm.getText();
+			
+			WebElement useSslElm = driver.findElement(By.cssSelector("span[title='gmailServer_useSsl']"));
+			String useSslBefore = useSslElm.getText();
+			
+			WebElement persistElm = driver.findElement(By.cssSelector("span[title='gmailServer_persistence']"));
+			String persistBefore = persistElm.getText();
+			
+			serverNameLink.click();
+			
+			logger.info("Edit page URL: " + driver.getCurrentUrl());
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.id("smtpedit:footer:gettingStartedFooter")));
+			
+			// View/Edit Detail Page
+			WebElement serverNameEdt = driver.findElement(By.id("smtpedit:content:servername"));
+			assertEquals(serverNameBefore, serverNameEdt.getAttribute("value"));
+			assertEquals(false, serverNameEdt.isEnabled());
+			
+			WebElement hostNameEdt = driver.findElement(By.id("smtpedit:content:smtphost"));
+			assertEquals(hostNameBefore, hostNameEdt.getAttribute("value"));
+			
+			WebElement userIdEdt = driver.findElement(By.id("smtpedit:content:userid"));
+			assertEquals(userIdBefore, userIdEdt.getAttribute("value"));
+			
+			Select selectUseSsl = new Select(driver.findElement(By.id("smtpedit:content:ssl")));
+			WebElement selectedUseSsl = selectUseSsl.getFirstSelectedOption();
+			assertEquals(useSslBefore, selectedUseSsl.getAttribute("value"));
+			
+			Select selectPersist = new Select(driver.findElement(By.id("smtpedit:content:persistence")));
+			WebElement selectedPersist = selectPersist.getFirstSelectedOption();
+			assertEquals(persistBefore, selectedPersist.getAttribute("value"));
+			if ("false".equals(persistBefore)) {
+				selectPersist.selectByValue("true");
+			}
+			else {
+				selectPersist.selectByValue("false");
+			}
+			
+			WebElement descEdt = driver.findElement(By.id("smtpedit:content:desc"));
+			String desc = descEdt.getAttribute("value");
+			if (StringUtils.endsWith(desc, "_updated")) {
+				desc = StringUtils.removeEnd(desc, "_updated");
+			}
+			else {
+				desc += "_updated";
+			}
+			descEdt.clear();
+			descEdt.sendKeys(desc);
+			
+			// submit changes
+			WebElement submit = driver.findElement(By.cssSelector("input[title='Submit changes']"));
+			submit.click();
+			
+			// accept (Click OK) JavaScript Alert pop-up
+			try {
+				WebDriverWait waitShort = new WebDriverWait(driver, 1);
+				Alert alert = (org.openqa.selenium.Alert) waitShort.until(ExpectedConditions.alertIsPresent());
+				alert.accept();
+				logger.info("Accepted the alert successfully.");
+			}
+			catch (org.openqa.selenium.TimeoutException e) { // when running HtmlUnitDriver
+				logger.error(e.getMessage());
+			}
+			
+			// verify the results
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.id("smtpsrvr:footer:gettingStartedFooter")));
+			
+			WebElement persistAfterElm = driver.findElement(By.cssSelector("span[title='gmailServer_persistence']"));
+			String persistAfter = persistAfterElm.getText();
+			if ("true".equals(persistBefore)) {
+				assertEquals("false", persistAfter);
+			}
+			else {
+				assertEquals("true", persistAfter);
+			}
+		}
+		catch (Exception e) {
+			logger.error("Exception caught", e);
+			fail();
+		}
+	}
+
+	@Test
 	public void testCopyFromSelected() {
 		logger.info("Current URL: " + driver.getCurrentUrl());
 		String listTitle = "Configure SMTP Servers";
@@ -111,4 +217,100 @@ public class SmtpServerTest extends AbstractLogin {
 		}
 	}
 
+	@Test
+	public void testAddNewServer() {
+		logger.info("Current URL: " + driver.getCurrentUrl());
+		String listTitle = "Configure SMTP Servers";
+		try {
+			WebElement link = driver.findElement(By.linkText(listTitle));
+			link.click();
+			
+			WebDriverWait wait = new WebDriverWait(driver, 5);
+			wait.until(ExpectedConditions.titleIs(listTitle));
+			
+			logger.info("Switched to URL: " + driver.getCurrentUrl());
+			
+			// Add a new record
+			WebElement copyToNewElm = driver.findElement(By.cssSelector("input[title='Add a new row']"));
+			copyToNewElm.click();
+			
+			logger.info("Edit page URL: " + driver.getCurrentUrl());
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.id("smtpedit:footer:gettingStartedFooter")));
+			
+			// Edit Detail Page
+			Random r = new Random();
+			String randomStr1 = StringUtils.leftPad(r.nextInt(1000) + "", 3, '0');
+			
+			WebElement serverName = driver.findElement(By.id("smtpedit:content:servername"));
+			serverName.clear();
+			serverName.sendKeys("testserver_" + randomStr1);
+			
+			WebElement hostName = driver.findElement(By.id("smtpedit:content:smtphost"));
+			hostName.clear();
+			hostName.sendKeys("localhost");
+			
+			WebElement userId = driver.findElement(By.id("smtpedit:content:userid"));
+			userId.clear();
+			userId.sendKeys("testuser_" + randomStr1);
+			
+			WebElement password = driver.findElement(By.id("smtpedit:content:password"));
+			password.clear();
+			password.sendKeys("testpswd_" + randomStr1 + ".local");
+			
+			WebElement desc = driver.findElement(By.id("smtpedit:content:desc"));
+			desc.clear();
+			desc.sendKeys("Test Host - " + randomStr1);
+			
+			WebElement submit = driver.findElement(By.cssSelector("input[title='Submit changes']"));
+			submit.click();
+			
+			// accept (Click OK) JavaScript Alert pop-up
+			try {
+				WebDriverWait waitShort = new WebDriverWait(driver, 1);
+				Alert alert = (org.openqa.selenium.Alert) waitShort.until(ExpectedConditions.alertIsPresent());
+				alert.accept();
+				logger.info("Accepted the alert successfully.");
+			}
+			catch (org.openqa.selenium.TimeoutException e) { // when running HtmlUnitDriver
+				logger.error(e.getMessage());
+			}
+			
+			// verify the results
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.id("smtpsrvr:footer:gettingStartedFooter")));
+			
+			String title = "testserver_" + randomStr1 + "_checkBox";
+			WebElement checkBoxLink = driver.findElement(By.cssSelector("input[title='" + title + "']"));
+			assertEquals(true, checkBoxLink.isDisplayed());
+			checkBoxLink.click();
+			
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[title='Delete selected rows']")));
+			wait.until(ExpectedConditions.elementSelectionStateToBe(By.cssSelector("input[title='" + title + "']"), true));
+			
+			// delete the added record
+			WebElement deleteLink = driver.findElement((By.cssSelector("input[title='Delete selected rows']")));
+			deleteLink.click();
+			
+			// accept (Click OK) JavaScript Alert pop-up
+			try {
+				WebDriverWait waitShort = new WebDriverWait(driver, 1);
+				Alert alert = (org.openqa.selenium.Alert) waitShort.until(ExpectedConditions.alertIsPresent());
+				alert.accept();
+				logger.info("Accepted the alert successfully.");
+			}
+			catch (org.openqa.selenium.TimeoutException e) { // when running HtmlUnitDriver
+				logger.error(e.getMessage());
+			}
+			
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.id("smtpsrvr:footer:gettingStartedFooter")));
+			wait.until(ExpectedConditions.invisibilityOfAllElements(driver.findElements(By.cssSelector("input[title='" + title + "']"))));
+			
+			// verify the record is deleted
+			List<WebElement> checkBoxs = driver.findElements(By.cssSelector("input[title='" + title + "']"));
+			assertTrue(checkBoxs.isEmpty());
+		}
+		catch (Exception e) {
+			logger.error("Exception caught", e);
+			fail();
+		}
+	}
 }
