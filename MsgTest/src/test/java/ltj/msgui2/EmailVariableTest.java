8e0c2cde055ad27f7fdf1abc73b353e84a0dcace
jwang <jwang@twang-pc.localdomain>
2017-05-01 19:41:20 -0400

    Signed-off-by: jwang <jwang@twang-pc.localdomain>

--- MsgTest/src/test/java/ltj/msgui2/EmailVariableTest.java
+++ MsgTest/src/test/java/ltj/msgui2/EmailVariableTest.java
@@ -1,101 +1,23 @@
-package ltj.selenium;
+package ltj.msgui2;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.BlockJUnit4ClassRunner;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import com.gargoylesoftware.htmlunit.javascript.host.css.CSS;
+public class EmailVariableTest extends AbstractLogin {
+	static final Logger logger = Logger.getLogger(EmailVariableTest.class);
 
-import junit.framework.TestCase;
-
-@RunWith(BlockJUnit4ClassRunner.class)
-public class LoginFlowTest extends TestCase {
-	static final Logger logger = Logger.getLogger(LoginFlowTest.class);
-
-	private static ChromeDriverService service;
-	private WebDriver driver;
-
-	@BeforeClass
-	public static void createAndStartService() throws IOException {
-		service = new ChromeDriverService.Builder().usingDriverExecutable(new File(OSUtil.getChromeDriverPath()))
-				.usingAnyFreePort().build();
-		service.start();
-	}
-
-	@AfterClass
-	public static void createAndStopService() {
-		service.stop();
-	}
-
-	@Before
-	public void createDriver() {
-		driver = new RemoteWebDriver(service.getUrl(), DesiredCapabilities.chrome());
-		login();
-	}
-
-	@After
-	public void quitDriver() {
-		driver.quit();
-	}
-
-	void login() {
-		try {
-			driver.get("http://localhost:8080/MsgUI2/login.faces");
-	
-			// Find the text input element by its name
-			WebElement element_user = driver.findElement(By.id("login:userid"));
-			element_user.sendKeys("admin");
-			
-			WebElement element_pswd = driver.findElement(By.name("login:password"));
-			element_pswd.sendKeys("admin");
-			
-			List<WebElement> inputs = driver.findElements(By.xpath("//input"));
-			for (WebElement we : inputs) {
-				logger.info("Input: " + we.toString());
-			}
-			
-			//element_pswd.submit();
-			driver.findElement(By.id("login:submit")).click();
-	
-			logger.info("Page title is: " + driver.getTitle());
-	
-			logger.info("Page URL is: " + driver.getCurrentUrl());
-			logger.info("Page source is: " + driver.getPageSource());
-	        
-			// wait until page is loaded
-			WebDriverWait wait = new WebDriverWait(driver, 5);
-			wait.until(ExpectedConditions.titleIs("Main Page"));
-		}
-		catch (Exception e) {
-			logger.error("Exception caught", e);
-			fail();
-		}
-	}
-	
 	@Test
 	public void testViewListAndCopy() {
 		logger.info("Current URL: " + driver.getCurrentUrl());
@@ -242,87 +164,5 @@ public class LoginFlowTest extends TestCase {
 			fail();
 		}
 	}
-	
-	/*
-	 Configure Site Profiles
-	 Configure POP/IMAP Accounts
-	 Configure SMTP Servers
-	 
-	 Customize Actions for Built-in Rules
-	 Setup Custom Bounce Rules and Actions
-	 Customize Action Details
-	 
-	 Setup Email Mailing Lists
-	 Upload Email Addresses to List
-	 Setup Email Variables
-	 Setup Email Templates
-	 
-	 Manage Email Correspondence
-	 Broadcast to a Mailing List
-	 Manage Email Addresses
-	 Manage Customer Information
-	 View Broadcast Messages
-	 
-	 Manage Site Users
-	 Change Password
-	 */
-	
-	
-	public static void main(String[] args) {
-		OSUtil.setupSeleniumBrowserDriver();
-		
-		WebDriver driver = new ChromeDriver();
-		//WebDriver driver = new FirefoxDriver();
-		
-		try {
-			driver.get("http://localhost:8080/MsgUI1/login.faces");
-	
-			// Find the text input element by its name
-			WebElement element_user = driver.findElement(By.id("login:userid"));
-			element_user.sendKeys("admin");
-			
-			WebElement element_pswd = driver.findElement(By.name("login:password"));
-			element_pswd.sendKeys("admin");
-			
-			List<WebElement> inputs = driver.findElements(By.xpath("//input"));
-			for (WebElement we : inputs) {
-				logger.info("Input: " + we.toString());
-			}
-			
-			// Now submit the form. WebDriver will find the form for us from the element
-			//element_pswd.submit();
-			driver.findElement(By.id("login:submit")).click();
-	
-			// Check the title of the page
-			logger.info("Page title is: " + driver.getTitle());
-			logger.info("Page URL is: " + driver.getCurrentUrl());
-			logger.info("Page source is: " + driver.getPageSource());
 
-			// wait until page is loaded
-			WebDriverWait wait = new WebDriverWait(driver, 5);
-			wait.until(ExpectedConditions.titleIs("Main Page"));
-			
-			logger.info("Current URL: " + driver.getCurrentUrl());
-		}
-		catch (Exception e) {
-			logger.error("Exception caught", e);
-		}
-		finally {
-			// Close the browser
-			driver.quit();
-		}
-	}
-	
-	/*
-	 * Start the ChromeDriver server separately before running tests: 
-	 * Terminal:
-		$ ./chromedriver
-		Started ChromeDriver
-		port=9515
-		version=14.0.836.0
-	 * 
-	 * and connect to it using the Remote WebDriver:
-	 * WebDriver driver = new RemoteWebDriver("http://127.0.0.1:9515", DesiredCapabilities.chrome());
-	 * driver.get("http://www.google.com");
-	 */
 }
