e0acd118e6a1a839b8714916d7b7df1d220f2c2a
wangj3 <wangj3@LNGRDUL-7009296.legal.regn.net>
2017-06-21 14:20:08 -0400

    Signed-off-by: wangj3 <wangj3@LNGRDUL-7009296.legal.regn.net>

--- MessageCore1/src/test/java/ltj/message/main/ProductKeyTest.java
+++ MessageCore1/src/test/java/ltj/message/main/ProductKeyTest.java
@@ -2,18 +2,21 @@ package ltj.message.main;
 
 import static org.junit.Assert.*;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import ltj.jbatch.common.ProductKey;
 import ltj.jbatch.obsolete.ProductUtil;
 
 public class ProductKeyTest {
+	static final Logger logger = Logger.getLogger(ProductKeyTest.class);
+	
 	static final String LF = System.getProperty("line.separator", "\n");
 	
 	@Test
 	public void testValidateKey() {
 		String prodKey = ProductUtil.getProductKeyFromFile();
-		System.out.println("Product key to validate: " + prodKey);
+		logger.info("Product key to validate: " + prodKey);
 		assertNotNull(prodKey);
 		
 		assertEquals(true, ProductKey.validateKey(prodKey));
